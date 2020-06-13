f5e4361baf4d23cc12b0b660b873329e5855f359
thelucvn <thelucqn@gmail.com>
2020-06-13 15:02:40 +0700

    13_06_2020
    ok

--- LaichauGIS_Data/Areas/Admin/Controllers/LoginController.cs
+++ LaichauGIS_Data/Areas/Admin/Controllers/LoginController.cs
@@ -3,6 +3,12 @@ using LaichauGIS_Data.Areas.Admin.Models;
 using Models;
 using System.Web.Mvc;
 using System.Web.Security;
+using Models.Framework;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System;
 
 namespace LaichauGIS_Data.Areas.Admin.Controllers
 {
@@ -38,6 +44,70 @@ namespace LaichauGIS_Data.Areas.Admin.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Login");
         }
-        
+        //GET: Admin/Login/ResetPassword
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+        //Post: Admin/Login/ResetPassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ResetPassword(string emailAddress)
+        {
+            if (emailAddress == "")
+            {
+                ModelState.AddModelError("", "Địa chỉ email không hợp lệ!");
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                LaichauDBContext context = new LaichauDBContext();
+                int res=await context.Database.SqlQuery<int>("select count(*) from UserAccount where emailAddress=@EmailAddress", new SqlParameter("@EmailAddress", emailAddress)).CountAsync();
+                if (res > 0)
+                {
+                   var resetPassword= await sendEmailResetPassword(emailAddress);
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+        }
+       async Task<bool> sendEmailResetPassword(string emailAddress)
+        {
+            var resetPass= Guid.NewGuid().ToString("d").Substring(1, 8);
+            LaichauDBContext context = new LaichauDBContext();
+            var loginName = await context.Database.SqlQuery<string>("Select loginName from UserAccount where emailAddress=@EmailAddress", new SqlParameter("@EmailAddress", emailAddress)).SingleOrDefaultAsync();
+            SqlParameter[] sqlParams = { new SqlParameter("@NewPassword", resetPass), new SqlParameter("@EmailAddress", emailAddress) };
+            var res = context.Database.ExecuteSqlCommand("Update UserAccount set loginPassword=@NewPassword where emailAddress=@EmailAddress", sqlParams);
+            if (res > 0)
+            {
+                var senderEmail = new MailAddress("ct0106.sys@gmail.com", "Admin email");
+                var receiverEmail = new MailAddress(emailAddress, "Receiver");
+                var password = "Humgct@0106";
+                var sub = "Khôi phục mật khẩu";
+                var body = "Đây là email khôi phục mật khẩu đề nghị không trả lời email này.\nTên đăng nhập: "+loginName+"\nMật khẩu mới của bạn: " + resetPass;
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(senderEmail.Address, password)
+                };
+                using (var mess = new MailMessage(senderEmail, receiverEmail)
+                {
+                    Subject = sub,
+                    Body = body
+                })
+                {
+                    smtp.Send(mess);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
     }
 }
