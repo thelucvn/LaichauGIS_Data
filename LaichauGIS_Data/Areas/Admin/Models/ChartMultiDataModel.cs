89d76c6a83c9af7345b7fef544ad02c38912dc1b
thelucvn <thelucqn@gmail.com>
2021-01-18 13:14:48 +0700

    18_01_2021
    ok

--- LaichauGIS_Data/Areas/Admin/Models/ChartMultiDataModel.cs
+++ LaichauGIS_Data/Areas/Admin/Models/ChartMultiDataModel.cs
@@ -8,10 +8,10 @@ namespace LaichauGIS_Data.Areas.Admin.Models
     public class ChartMultiDataModel
     {
         public DateTime x { get; set; }
-        public double y1 { get; set; }
-        public double y2 { get; set; }
-        public double y3 { get; set; }
-        public double y4 { get; set; }
-        public double y5 { get; set; }
+        public Nullable<double> y1 { get; set; }
+        public Nullable<double> y2 { get; set; }
+        public Nullable<double> y3 { get; set; }
+        public Nullable<double> y4 { get; set; }
+        public Nullable<double> y5 { get; set; }
     }
 }
--- LaichauGIS_Data/Areas/Admin/Models/DataSummary.cs
+++ LaichauGIS_Data/Areas/Admin/Models/DataSummary.cs
@@ -9,8 +9,8 @@ namespace LaichauGIS_Data.Areas.Admin.Models
     {
         public int dataIndex { get; set; }
         public string mLocationName { get; set; }
-       public double lastMeasuredData { get; set; }
-        public double averageInWeek { get; set; }
-        public double averageInMonth { get; set; }
+       public Nullable<double> lastMeasuredData { get; set; }
+        public Nullable<double> averageInWeek { get; set; }
+        public Nullable<double> averageInMonth { get; set; }
     }
 }
